0f59203b87c3e6d938751f0aa952043665a0c7e3
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2021-07-26 17:03:11 +0300

    routing update

--- src/app/modules/website/website-routing.module.ts
+++ src/app/modules/website/website-routing.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RootPage } from 'src/app/pages/root/root.page';
+import { RootPage } from 'src/app/modules/website/pages/root/root.page';
 import { LessonsPage } from './pages/lessons/lessons.page';
 import { MainMenuPage } from './pages/main-menu/main-menu.page';
+import { MainPage } from './pages/main/main.page';
 import { OptionsPage } from './pages/options/options.page';
 import { ProfilePage } from './pages/profile/profile.page';
 
 const routes: Routes = [
   {
-    path: '', component: RootPage, children: [
+    path: '', component: RootPage, children: [      
       { path: 'anamenu', component: MainMenuPage },
       { path: 'profil', component: ProfilePage },
       { path: 'dersler', component: LessonsPage },
-      { path: 'ayarlar', component: OptionsPage }
+      { path: 'ayarlar', component: OptionsPage },
+      { path: '', component: MainPage }   
     ]}
     ];
 
