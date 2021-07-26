eed07e4cb0316d6365de783dd40ff47418a870eb
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2021-07-26 16:24:19 +0300

    routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainMenuPage } from './modules/website/pages/main-menu/main-menu.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'website', loadChildren: () => import('./modules/website/website.module').then(m=> m.WebsiteModule)},
+  {path: 'website/:', loadChildren: () => import('./modules/website/website.module').then(m=> m.WebsiteModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/website/website-routing.module.ts
+++ src/app/modules/website/website-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RootPage } from 'src/app/pages/root/root.page';
+import { LessonsPage } from './pages/lessons/lessons.page';
+import { MainMenuPage } from './pages/main-menu/main-menu.page';
+import { OptionsPage } from './pages/options/options.page';
+import { ProfilePage } from './pages/profile/profile.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: RootPage, children: [
+      { path: 'anamenu', component: MainMenuPage },
+      { path: 'profil', component: ProfilePage },
+      { path: 'dersler', component: LessonsPage },
+      { path: 'ayarlar', component: OptionsPage }
+    ]}
+    ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
-})
+      imports: [RouterModule.forChild(routes)],
+      exports: [RouterModule]
+    })
 export class WebsiteRoutingModule { }
