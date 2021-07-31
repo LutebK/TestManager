c6a00891842cb42cb8ce66aacd293c1e48011bac
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2021-07-31 18:13:58 +0300

    root page header and backbutton routing update

--- src/app/modules/website/pages/root/root.page.ts
+++ src/app/modules/website/pages/root/root.page.ts
@@ -1,4 +1,7 @@
+import { areAllEquivalent } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'website-root',
@@ -8,23 +11,38 @@ import { Component, OnInit } from '@angular/core';
 export class RootPage implements OnInit {
 
   mainMenuPath = true;
-  mainMenuPath2=false;
+  mainMenuPath2 = false;
   mainMenuOut = false;
-  constructor() { }
+  a: any;
+  b: any;
+  array: any[] = [];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    
   }
 
-  setPath(){
+  setPath() {
     this.mainMenuPath = !this.mainMenuPath;
     this.mainMenuPath2 = !this.mainMenuPath2;
-    if(this.mainMenuPath2)
-      document.getElementById("projectsection").style.display="none";
+    if (this.mainMenuPath2)
+      document.getElementById("projectsection").style.display = "none";
     else
-      document.getElementById("projectsection").style.display="block";  
+      document.getElementById("projectsection").style.display = "block";
   }
-  setheader(){
+  setheader() {
     this.mainMenuOut = !this.mainMenuOut;
   }
-  
+  getUrl() {
+    return window.location.pathname.slice(window.location.pathname.lastIndexOf("/") + 1)
+  }
+  getBackUrl() {
+    this.array = this.router.url.split("/")
+    this.array.pop()
+    let url = ""
+    this.array.forEach(a =>  this.array[this.array.length - 1] == a ? url = url + a : url = url + a + "/" );
+    this.router.navigateByUrl(url)
+
+  }
 }
