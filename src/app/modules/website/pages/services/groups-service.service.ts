4feed6b7ec5a45d08a5b31f799dce492065a8ab0
ifurkanturkoglu <turkoglu.ifurkan@gmail.com>
2021-08-09 16:31:21 +0300

    groups update

--- src/app/modules/website/pages/services/groups-service.service.ts
+++ src/app/modules/website/pages/services/groups-service.service.ts
@@ -5,6 +5,7 @@ import { AngularFirestore } from '@angular/fire/firestore';
 
 import { resolveSanitizationFn } from '@angular/compiler/src/render3/view/template';
 import { environment } from 'src/environments/environment';
+import { tr } from 'date-fns/locale';
 
 
 
@@ -12,46 +13,55 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class GroupsServiceService {
-
-  inviteCodeGet1:any;
-  firebaseId:string=""
-  constructor(private firestore: AngularFirestore,private aut:AngularFireAuth) {
-
-  }
-  async users(){
-    return await this.firestore.collection("/users/").get()
-  }
-
-  async deneme(){
-    return await this.firestore.collectionGroup("options",ref=> ref.where("invite","==",this.inviteCodeGet1)).get()
-  }
-
-
-  async userInviteCode(){
-    return await this.firestore.doc("/users/"+ (await this.aut.currentUser).uid+"/options/optionInfo").get()
-  }
-  async userInviteCode2(id:String){
-    return await this.firestore.doc("/users/"+ id+"/options/optionInfo").get()
-  }
-
-  async userGets() {
-    return await this.firestore.collection('/users/'+ (await this.aut.currentUser).uid+'/groups/userAdded/addId').get()
-  }
-  async userGet(){
-    return await this.firestore.doc('/users/'+ (await this.aut.currentUser).uid+'/profile/profileInfo').get();
-  }
-
-  inviteCodeGet(code:any){
-    this.inviteCodeGet1=code
-  }
-  async userAdd(id: GroupsInformation){
-    return await this.firestore.collection('/users/').doc((await this.aut.currentUser).uid).collection('/groups/').doc('userAdded').update(id)
-}
-  async getPicture(){
-    return await this.firestore.collection('/users/'+ (await this.aut.currentUser).uid+'profile/profileInfo').get()
-  }
-  async userDbInsert(){
+  getGroupsId: any = "";
+  userPath: any = ""
+  firebaseId: string = ""
+  constructor(private firestore: AngularFirestore, private aut: AngularFireAuth) {
 
   }
 
+  getGroupsTsID(id: any) {
+    return this.getGroupsId = id
+  }
+
+  async sendUserPath(path:any){
+    this.userPath=path
+  }
+
+
+  async user() {
+    return await this.firestore.doc("/users/" + this.userPath + "/profile/profileInfo").get()
+  }
+
+  async userİnviteCode() {
+    return await this.firestore.collectionGroup("options", ref => ref.orderBy("invite"))
+  }
+
+  async findUserPath() {
+    return await this.firestore.collectionGroup("options", ref => ref.where("invite", "==", this.getGroupsId)).get()
+  }
+
+  async usersAdded(users:GroupsInformation){
+    return await this.firestore.collection('/users/').doc((await this.aut.currentUser).uid).collection('/groups').add(users)
+  }
+
+  async usersGetInfo(){
+    return await this.firestore.collection('/users/'+ (await this.aut.currentUser).uid+"/groups").get()
+  }
+
+  // async userGets() {
+  //   return await this.firestore.collection('/users/' + (await this.aut.currentUser).uid + '/groups/userAdded/addId').get()
+  // }
+
+  async userGet() {
+    return await this.firestore.doc('/users/' + (await this.aut.currentUser).uid + '/profile/profileInfo').get();
+  }
+
+  // async userAdd(id: GroupsInformation) {
+  //   return await this.firestore.collection('/users/').doc((await this.aut.currentUser).uid).collection('/groups/').doc('userAdded').update(id)
+  // }
+
+  async getPicture() {
+    return await this.firestore.collection('/users/' + (await this.aut.currentUser).uid + 'profile/profileInfo').get()
+  }
 }
