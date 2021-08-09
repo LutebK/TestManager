efb0a625dc5492d76e38edbffe80cf2b0a0a5ca2
HasanHuseyinGumustas <hasangumustas@gmail.com>
2021-08-10 00:56:33 +0300

    test fonksiyonları

--- src/app/models/tests-information.model.ts
+++ src/app/models/tests-information.model.ts
@@ -2,7 +2,17 @@ export class TestsInformation {
     public questionNumber: number;
     public questionImg: string;
     public answer : string;
-    public testID : string;
     public questionUrl : string;
     
+}
+
+export class TestsDocument{
+    public testId: string
+    public testInformation: TestsInformation
+
+    constructor(testInformation: TestsInformation , testId: string){
+
+        this.testId = testId;
+        this.testInformation = testInformation;
+    }
 }
--- src/app/modules/website/pages/services/lessons-services.service.ts
+++ src/app/modules/website/pages/services/lessons-services.service.ts
@@ -3,12 +3,33 @@ import { FirebaseApp } from '@angular/fire';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { getHeapCodeStatistics } from 'v8';
 import { resolveSanitizationFn } from '@angular/compiler/src/render3/view/template';
-import { TestsInformation } from 'src/app/models/tests-information.model';
+import { TestsDocument, TestsInformation } from 'src/app/models/tests-information.model';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LessonsServicesService {
 
-  constructor() { }
+  constructor(private storage: AngularFireStorage, private afs: AngularFirestore) { }
+
+  async createStorage(testData: TestsInformation) {
+
+    return await this.afs.collection('/Tests').add(testData)
+  }
+  //Storage klasörlerinin oluşturulması için gerekli fonksiyon
+  async getTest(testInf: TestsDocument) {
+    return await this.afs.doc('/Tests/' + testInf.testId).get().subscribe((res: any) => { testInf.testId = res.id; testInf.testInformation = res.data(); console.log(res.data()) })
+  }
+  //Storage klasörlerinin tekli okunması için fonksiyon
+
+async getTests(){
+  return await this.afs.collection('/Tests').get()
+}
+
+  async updateStorage(testInf: TestsDocument) {
+return await this.storage.upload('/Tests' , testInf.testId)
+  }
+  //Storage klasörlerinin güncellenmesi için fonksiyon
 }
